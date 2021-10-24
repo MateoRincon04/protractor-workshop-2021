f86ce1c6a9382e36b57a78a9227b9bf9be8eaa16
MateoRincon04 <marinconar@unal.edu.co>
2021-10-24 16:59:35 -0500

    improving first test

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,9 +1,12 @@
-import { Config } from 'protractor';
+import { Config, browser } from 'protractor';
 
 export const config: Config = {
 	  framework: 'jasmine',
 	    specs: [ '../test/google.spec.js' ],
 	      seleniumAddress: 'http://localhost:4444/wd/hub',
+        onPrepare: () => {
+          browser.ignoreSynchronization = true;
+      },
         capabilities: {
           browserName: 'chrome',
           count: 1,
